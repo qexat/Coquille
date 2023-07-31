25712aacec6a030a352c8d1784fe5772fff551fb
qexat <contact@qexat.com>
2023-07-31 15:24:28 +0200

    Allow the use of sequence names

--- examples/coquille_context/__main__.py
+++ examples/coquille_context/__main__.py
@@ -1,14 +1,11 @@
 from coquille import Coquille
-from coquille.sequences import bold
-from coquille.sequences import fg_magenta
-from coquille.sequences import italic
 
 print("Hello World!")
 
 # By default, the coquille wraps the standard output
-with Coquille.new(fg_magenta, italic) as coquille:
+with Coquille.new("fg_magenta", "italic") as coquille:
     print("Hello World, but in magenta and italic!")
-    coquille.apply(bold)
+    coquille.apply("bold")
     print("Now, with a touch of bold :D")
 
 print("Oh, we are back to normal now...")
--- tests/coquille_test.py
+++ tests/coquille_test.py
@@ -1,9 +1,10 @@
+import pytest
 from io import StringIO
 
-import pytest
-from coquille.coquille import *
+from coquille.prelude import *
 from coquille.sequences import DEC_save_cursor
 from coquille.sequences import erase_in_display
+from coquille.sequences import fg_black
 from coquille.sequences import select_graphical_rendition
 from coquille.sequences import start_of_string
 
@@ -12,6 +13,7 @@ from coquille.sequences import start_of_string
     ["args", "output"],
     [
         ((DEC_save_cursor,), DEC_save_cursor),
+        (("fg_black",), fg_black),
         ((start_of_string,), "\u001bX"),
         ((erase_in_display, 2), "\u001b[2J"),
         ((select_graphical_rendition, 38, 5, 16), "\x1b[38;5;16m"),
@@ -26,6 +28,7 @@ def test_prepare(args, output):
     [
         (DEC_save_cursor, (), DEC_save_cursor),
         (start_of_string, (), "\u001bX"),
+        ("fg_black", (), fg_black),
         (erase_in_display, (2,), "\u001b[2J"),
         (select_graphical_rendition, (38, 5, 16), "\x1b[38;5;16m"),
     ],
